aacc011cb734c45fb5ac3d0bea55ea05516ba815
Soham Ghosh <sohamghosh@gmail.com>
2013-12-07 10:35:09 +0530

    Initial Commit

---
+++ NSCoding/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NSCoding
+//
+//  Created by Soham on 07/12/13.
+//  Copyright (c) 2013 TW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NSCoding/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NSCoding
+//
+//  Created by Soham on 07/12/13.
+//  Copyright (c) 2013 TW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
